9c5fac992c10eec793514e741d06ad2c10835a73
Mohamed MotYim <mohamed.motyim@gmail.com>
2020-06-03 14:10:25 +0200

    demo commit

---
+++ src/main/java/com/sas/sanctiondemo/SanctionDemoApplication.java
@@ -0,0 +1,22 @@
+package com.sas.sanctiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SanctionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanctionDemoApplication.class, args);
+    }
+
+    @GetMapping("/welcome")
+    public String getWelcome(){
+        return  "Welcome";
+    }
+
+
+}
